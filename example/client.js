0157cefc5c8c4aace24e3b87774e74fa05ef21f5
Tomas Bosak <bosak.tomas@gmail.com>
2012-08-10 18:37:11 +0200

    Add more test buddies.

--- example/client.js
+++ example/client.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
     var actor1 = new Walker({
         element: $("#actor1"),
         sprite: "../sprites/lemming.png",
-        columnsCount: 8,
-        currentRow: 0,
         start: {
             x: 0,
             y: 0
@@ -14,10 +12,26 @@ $(document).ready(function() {
     var actor2 = new Walker({
         element: $("#actor2"),
         sprite: "../sprites/lemming.png",
-        columnsCount: 8,
-        currentRow: 0,
         start: {
             x: 128,
+            y: 128
+        }
+    });
+    
+    var actor3 = new Walker({
+        element: $("#actor3"),
+        sprite: "../sprites/lemming.png",
+        start: {
+            x: 165,
+            y: 128
+        }
+    });
+    
+    var actor4 = new Walker({
+        element: $("#actor4"),
+        sprite: "../sprites/lemming.png",
+        start: {
+            x: 200,
             y: 128
         }
     });
